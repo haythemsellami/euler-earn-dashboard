afd4e74d9615132b40b938f866b3ac60c0debecf
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2025-01-22 11:33:15 +0700

    update

--- app/config/wagmi.ts
+++ app/config/wagmi.ts
@@ -1,8 +1,8 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { getDefaultConfig, type Chain as RainbowChain } from '@rainbow-me/rainbowkit'
 import { SUPPORTED_NETWORKS } from './addresses'
 import { http, type Chain } from 'viem'
 import { type Config, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { mainnet, sepolia, type Chain as WagmiChain } from 'wagmi/chains'
 
 // Convert supported networks to wagmi chains
 const customChains = Object.entries(SUPPORTED_NETWORKS).reduce<Chain[]>((acc, [chainId, network]) => {
@@ -37,8 +37,8 @@ const chains = [mainnet, sepolia, ...customChains]
 
 const config = getDefaultConfig({
   appName: 'Euler Earn',
-  projectId: '1533286dc78d5ee31cd5d96f99701d47',
-  chains,
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: chains as unknown as [RainbowChain, ...RainbowChain[]],
   ssr: true
 })
 
